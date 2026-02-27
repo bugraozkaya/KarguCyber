68a75ed24306e594d3575ad4bc4ce0ff6f75e7c6
bugraozkaya <ahmetbugraozkaya14@gmail.com>
2026-02-27 09:17:49 +0300

    Update settings.gradle.kts

--- kargu_cyber_mobile/android/settings.gradle.kts
+++ kargu_cyber_mobile/android/settings.gradle.kts
@@ -2,9 +2,12 @@ pluginManagement {
     val flutterSdkPath =
         run {
             val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
+            val propertiesFile = file("local.properties")
+            if (propertiesFile.exists()) {
+                propertiesFile.inputStream().use { properties.load(it) }
+            }
             val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties. Please run 'flutter pub get' in the project root." }
             flutterSdkPath
         }
 
@@ -19,8 +22,8 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false  // <--- SADECE BURAYI 2.2.0 YAPTIK
 }
 
 include(":app")
