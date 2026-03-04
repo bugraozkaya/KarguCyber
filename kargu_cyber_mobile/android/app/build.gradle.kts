11237c2c1da26b84fe2e61ca06347ad9c08ec013
bugraozkaya <ahmetbugraozkaya14@gmail.com>
2026-03-04 13:24:55 +0300

    Update build.gradle.kts

--- kargu_cyber_mobile/android/app/build.gradle.kts
+++ kargu_cyber_mobile/android/app/build.gradle.kts
@@ -3,19 +3,21 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") // <--- İŞTE BU SATIRI EKLEDİK
 }
 
 android {
     namespace = "com.example.kargu_cyber_mobile"
-    
+
     // --- BURAYI DEĞİŞTİRDİK VE EKLEDİK ---
     compileSdk = 36
     buildToolsVersion = "34.0.0"
     // ------------------------------------
-    
+
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true // <--- YENİ EKLENEN SATIR: Hata çözümünün kalbi
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -27,11 +29,11 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.kargu_cyber_mobile"
-        
+
         // --- BURAYI GÜVENCEYE ALDIK ---
-        minSdk = flutter.minSdkVersion 
+        minSdk = flutter.minSdkVersion
         // ------------------------------
-        
+
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -48,4 +50,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+// <--- SADECE SÜRÜMÜ 2.1.4 YAPTIK --->
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
